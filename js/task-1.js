f53a246521fb2646f97eae9347a8776f04b7b3a2
Yana_Levchenko <yanalevchenko@gmail.com>
2026-03-01 17:22:02 +0100

    tasks

--- js/task-1.js
+++ js/task-1.js
@@ -25,3 +25,9 @@ const customer = {
     this.orders.push(order);
   },
 };
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
