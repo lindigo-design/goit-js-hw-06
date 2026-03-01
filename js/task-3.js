0b8f5045872add43f3cc7d132c3a87a3152da514
Yana_Levchenko <yanalevchenko@gmail.com>
2026-03-01 17:31:44 +0100

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -21,3 +21,12 @@ class StringBuilder {
     this.#value = str + this.#value + str;
   }
 }
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
